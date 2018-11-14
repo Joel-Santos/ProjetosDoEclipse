d7d5edf6f3c3b2d88e5404ddfbf4f26745fb2113
Joel Santos <joeloliveira56@gmail.com>
2018-11-14 08:56:27 -0300

    Update SomaConvolucao.java
    Teste da função de Normalização

--- ProjetoAG/src/testes/SomaConvolucao.java
+++ ProjetoAG/src/testes/SomaConvolucao.java
@@ -1,4 +1,8 @@
 package testes;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -17,12 +21,37 @@ public class SomaConvolucao {
    
         for(int i = 0; i < soma.length; i++){
             for(int j = 0; j < h.length; j++){
-                if((i-j) >= 0 && (i-j) < x.length)
+                if((i-j) >= 0 && (i-j) < x.length){
                     soma[i] += h[j] * x[i-j]; 
+                    	
+                    
+                }
             }
         }
         return soma;
     }
+    
+	
+	public static int[] normalizar(int[] soma){
+		int[] ConvNormalizada = new int[soma.length];
+		int min = Arrays.stream(soma).min().getAsInt();
+        int max = Arrays.stream(soma).max().getAsInt();
+		
+		for(int i=0; i<ConvNormalizada.length; i++){
+			
+			ConvNormalizada[i] = Math.abs((soma[i]-min)/(max-min));
+			
+		}
+			
+		return ConvNormalizada;
+	}
+	
+    
+    
+    
+    
+    
+    
     
     /**
      * @param args the command line arguments
@@ -31,12 +60,74 @@ public class SomaConvolucao {
         
         
         // TODO code application logic here
-        int[] x = new int[]{177,40}; 
-        int[] h = new int[]{134,78}; 
+        int[] x = new int[]{1,1,0,1,0,1,1,0}; 
+        int[] h = new int[]{0,1,0,0,1,1,0,1}; 
         int[] soma = somaConvolucao(x, h);
+        
+        int[] ConvNormalizada = normalizar(soma);
+        
+        
         for(int i : soma)
-            System.out.println(i);
-/*
+            System.out.print(i);
+
+        
+        
+        System.out.println("---");         
+        
+        System.out.println("\nSaida normalizada");
+        for(int i : ConvNormalizada)
+        System.out.print(i);
+        
+        
+        
+        
+        int[] filho0 = new int[8]; 
+        for(int i=0; i<filho0.length;i++)
+        	filho0[i] = soma[i];
+        
+        int min = Arrays.stream(soma).min().getAsInt();
+        int max = Arrays.stream(soma).max().getAsInt();
+        
+        System.out.println("\nmin = "+min);
+        System.out.println("max = "+max);
+        
+   //     System.arraycopy(soma,0,filho0,0,7);
+        
+        int[] filho1 = new int[8]; 
+      //  System.arraycopy(soma,7,filho2,0,7);        
+ 
+        int j=soma.length/2;
+        for(int i=0; i<filho1.length; i++, j++)
+        	filho1[i] = soma[j];
+ 
+        int[] filho2 = new int[8]; 
+        //  System.arraycopy(soma,7,filho2,0,7);        
+   
+          int k=4;
+          for(int i=0; i<filho2.length; i++, k++)
+          	filho2[i] = soma[k];
+   
+            
+        
+          
+      System.out.println("FILHO0");  
+        for(int i : filho0)
+            System.out.print(i);
+        
+     
+       System.out.println("---");  
+        System.out.println("FILHO1"); 
+        for(int i : filho1)
+            System.out.print(i);
+        
+        System.out.println("---");  
+        System.out.println("FILHO2"); 
+        for(int i : filho2)
+            System.out.print(i);  
+        
+        
+       
+        /*
 
 	r[0] ={3,2} --> 1*3 , 1*2 
 	r[1] ={0,9,6} --> 3*3 , 3*2
